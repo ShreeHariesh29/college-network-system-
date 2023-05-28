1e1e548c102c1ab264ded9f783bd093158569c8b
guhan1203 <guhanramu1203@gmail.com>
2023-05-28 23:31:58 +0530

    top

--- student_management/src/components/HomeComponents/TopAnnouncement.jsx
+++ student_management/src/components/HomeComponents/TopAnnouncement.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function TopAnnouncement() {
   return (
-    <div className='h-[60px] w-[300px] shadow-lg bg-white rounded-lg mt-[30px] mr-[20px] border-2'>
-      <h1 className='pt-5 pl-[200px]'>3.00%</h1>
-      
-    </div>
+    <div className='flex border-2 h-[60px] w-[300px] shadow-lg bg-white  rounded-lg mt-[30px] '>
+    <div><img className="h-[40px] w-[40px] mt-2 ml-2"src="https://cdn-icons-png.flaticon.com/512/273/273491.png" alt="" /></div>
+    <div className='pt-1 pl-[20px] text-[30px]'><b>00</b></div>
+  <div><h1 className='pt-4 pl-[20px]'>ANNOUNCEMENT</h1></div>
+</div>
   )
 }
 
