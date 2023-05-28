0f995b7c97ec135cbbdca4fb20ff20714f2c4467
Bhavan663 <venkateshbhavankalyan@gmail.com>
2023-05-29 00:31:19 +0530

    New commit

--- student_management/src/pages/Examination.jsx
+++ student_management/src/pages/Examination.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Examination() {
   return (
-    <div>Examination</div>
+    <div className='flex justify-between'>
+      <div><img className='h-[250px] w-[300px] mt-[20px] pl-[35px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvYXb7VlC1YgBNOnhPjKpuKMSiktqezTZz5A&usqp=CAU" alt="hi" />
+    </div>
+    <div className='w-full shadow-lg '>
+    <div><h1 className='pt-[10px] font-black text-2xl font-serif text-center'> Mai-San</h1>
+      </div>
+<div className='ml-[20px]'>
+<h2 className='mt-[10px] font-bold font-serif '>DEPARTMENT<span className='font-medium font-sans'> : CSE</span></h2>
+    <h2 className='mt-[10px] font-bold font-serif'>YEAR<span className='font-medium font-sans pl-[77px]'> : III</span></h2>
+    <h2 className='mt-[10px] font-bold font-serif'>SEMESTER<span className='font-medium pl-[34px] font-sans'>: 06</span></h2>
+    <h2 className='mt-[10px] font-bold font-serif'>BATCH<span className='font-medium pl-[69px] font-sans'>: 2020-2024</span></h2>
+  </div>    
+    </div>
+
+    </div>
+    
   )
 }
 
--- student_management/src/pages/Home.jsx
+++ student_management/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/HomeComponents/Navbar'
+import Attendance from '../components/HomeComponents/Attendance'
+import QuickAccess from '../components/HomeComponents/QuickAccess'
+import Notice from '../components/HomeComponents/Notice.jsx'
+import TopAnnouncement from '../components/HomeComponents/TopAnnouncement'
+import TopAssingment from '../components/HomeComponents/TopAssingment'
+import TopAttendance from '../components/HomeComponents/TopAttendance'
+import TimeTable from '../components/HomeComponents/TimeTable'
 
 function Home() {
   return (
-    <Navbar />
+    <div>
+        <div className='flex justify-between ml-[30px] mr-[30px] '>
+          <TopAnnouncement />
+          <TopAssingment />
+          <TopAttendance />
+        </div>
+        <div className='flex pr-[10px]'>
+        <div><Attendance /></div>
+        <div><QuickAccess /></div>
+        <div><Notice /></div>
+        </div>
+        <div><TimeTable /></div>
+        
+    </div>
+    
   )
 }
 
-export default Home
+export default Home
