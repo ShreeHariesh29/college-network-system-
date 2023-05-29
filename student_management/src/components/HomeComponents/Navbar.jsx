bedb7d7795545e25942597a44227f1d997f9de7a
Shree Hariesh.M <shreehariesh29@gmail.com>
2023-05-29 20:13:06 +0530

    ,,,,

--- student_management/src/components/HomeComponents/Navbar.jsx
+++ student_management/src/components/HomeComponents/Navbar.jsx
@@ -31,11 +31,11 @@ function Navbar() {
             </div>
             
         </nav>
-        <div className='pt-[7px] pb-[8px]'>
+        <div className='pt-[5px] '>
                 <ul className='flex'>
                     <input className='rounded-lg pl-[8px]' placeholder='search' type="text" />
-                    <li className='p-[5px] w-[50px]'></li>
-                    <li className='p-[5px] w-[70px]'> <Link to="/profile"><img src="/images/profile.png" alt="" /></Link> </li>
+                    <li className='w-[70px]'></li>
+                    <li className=' w-[70px]'> <Link to="/profile"><img src="/images/profile.png" alt="" /></Link> </li>
                 </ul>
                 
             </div>
